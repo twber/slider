c997a34cbb8fe6720f3e837419b6d5984e1655f2
twber <hctsai@umich.edu>
2020-08-12 15:14:20 +0800

    The rabbit chases a carrot.

--- slider/ViewController.swift
+++ slider/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MoveSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var rightView: UIImageView!
+    @IBOutlet weak var leftView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func distanceShift(_ sender: UISlider) {
+        //sender.setValue(sender.value.rounded(), animated: true)
+           numberLabel.text = Int(sender.value).description
+           numberLabel.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0)
+        //alpha
+        leftView.alpha = CGFloat( sender.value / sender.maximumValue)
+        rightView.alpha = CGFloat( sender.value / sender.maximumValue)
+        //shift
+        //leftView.transform = CGAffineTransform(translationX: 20 * sender.value, y: 0)
+        //rightView.transform = CGAffineTransform(translationX: -20 * sender.value, y: 0)
+        let index = MoveSegmentedControl.selectedSegmentIndex
+        switch index {
+        case 0:
+            leftView.frame.origin.x = CGFloat(0 + 10 * sender.value * 1.8)
+            rightView.frame.origin.x = CGFloat(480 - 10 * sender.value * 0.2)
+        case 1:
+            leftView.frame.origin.x = CGFloat(0 + 10 * sender.value)
+            rightView.frame.origin.x = CGFloat(480 - 10 * sender.value)
+        default:
+            return
+        }
+    }
+    
 }
 
